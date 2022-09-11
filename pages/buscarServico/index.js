860e75f2a8514a463578a5d724ab696dcf6dd9e6
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-09-11 11:41:24 -0300

    ending the service list

---
+++ pages/buscarServico/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import SearchServiceComponent from "../../components/pages/buscarServico";
+
+export default function searchService() {
+  return (
+    <div>
+        <SearchServiceComponent/>
+    </div>
+  );
+}
