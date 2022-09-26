c0e9cf2f414f69c8650e2bde7e9c258038957ab2
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2022-09-25 23:54:12 -0300

    finalizando dashboard de usuario

--- components/agendamentos/index.js
+++ components/agendamentos/index.js
@@ -3,7 +3,7 @@ import styles from "../../styles/Agendamentos.module.css";
 import DialogCancel from "../dialogCancel";
 import DialogAdd from "../dialogAdd";
 
-export default function Agendamentos({ children }) {
+export default function Agendamentos({ usuario }) {
     const [listService, setListService] = useState(
         [
             {
@@ -26,34 +26,48 @@ export default function Agendamentos({ children }) {
             }
         ]
     );
-    
-  return (
-    <div className={styles.divContent}>
-        <div>
-            <div className={styles.subTitle}>Agendamentos Pendentes</div>
-            {listService.map(item => (
-                <div className={styles.divButton}>
-                    {item.service} - {item.name} - {item.day} - {item.hour}
-                    <div className={styles.accept}>
-                        <DialogAdd/>
-                        <DialogCancel/>
-                    </div>
-                </div>
-            ))}
-        </div>
 
-        <div>
-            <div className={styles.subTitle}>Agendamentos Confirmados</div>
-            {listService.map(item => (
-                <div className={styles.divButton}>
-                    {item.service} - {item.name} - {item.day} - {item.hour}
-                    <div className={styles.accept}>
-                        <div></div>
-                        <DialogCancel/>
+    return (
+        <div className={styles.divContent}>
+
+            {usuario &&
+
+                <div style={{ marginTop: 40 }}>
+                    <div className={styles.subTitle}>Procurar Serviços</div>
+                    <div className={styles.divButton}>
+                        Busque serviços
+                        <div className={styles.buscar}>
+                            buscar
+                        </div>
                     </div>
                 </div>
-            ))}
+            }
+
+            <div>
+                <div className={styles.subTitle}>Agendamentos Pendentes</div>
+                {listService.map(item => (
+                    <div className={styles.divButton}>
+                        {item.service} - {item.name} - {item.day} - {item.hour}
+                        <div className={styles.accept}>
+                            <DialogAdd />
+                            <DialogCancel />
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <div className={styles.subTitle}>Agendamentos Confirmados</div>
+                {listService.map(item => (
+                    <div className={styles.divButton}>
+                        {item.service} - {item.name} - {item.day} - {item.hour}
+                        <div className={styles.accept}>
+                            <div></div>
+                            <DialogCancel />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
